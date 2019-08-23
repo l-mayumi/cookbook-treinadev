61ccc59c6b3ae0654d529d41189a9ac0fb83d118
Leticia Mayumi <hashizumemay@gmail.com>
2019-08-23 01:52:25 -0300

    Usuario usa tipos de receitas cadastradas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,39 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def new
+  	@recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+  	else
+      flash[:alert] = 'Não foi possível salvar a receita'
+      render :new
+    end
+  end
+
+  def edit
+  	@recipe = Recipe.find(params[:id])
+  end
+
+  def update
+  	@recipe = Recipe.find(params[:id])
+  	if @recipe.update(recipe_params)
+  	  redirect_to @recipe
+  	else
+  	  flash[:alert] = 'Não foi possível salvar a receita'
+      render :edit
+  	end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type_id, :cuisine, :difficulty,
+                                   :cook_time, :ingredients, :cook_method)
+
+  end
 end
